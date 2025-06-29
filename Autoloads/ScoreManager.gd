95108fb8252c9e689e40591d2a83251c441a5fc7
KodeDino <15905922+kodedino@user.noreply.gitee.com>
2025-06-28 20:21:25 -0700

    feat: add score system and label

---
+++ Autoloads/ScoreManager.gd
@@ -0,0 +1,8 @@
+extends Node
+
+
+var _score: int = 0
+
+
+func increment_score() -> void:
+	_score += 1
---
+++ Scenes/Hud/hud.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+@onready var score_label: Label = $MarginContainer/ScoreLabel
+
+
+func _ready() -> void:
+	update_score_label()
+	SignalManager.on_basketball_score.connect(_on_basketball_score)
+
+
+func _on_basketball_score() -> void:
+	update_score_label()
+
+
+func update_score_label() -> void:
+	score_label.text = str(ScoreManager._score)
