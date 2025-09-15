14a5dbbfbd063e6863fa04de5cd9a1d6f9255f98
KodeDino <yochen0302@gmail.com>
2025-09-15 16:10:20 -0700

    chore: add background to game over ui

--- Scenes/UI/GameOver/GameOver.gd
+++ Scenes/UI/GameOver/GameOver.gd
@@ -1,6 +1,16 @@
 extends Control
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var game_over_label: Label = $GameOverLabel
+@onready var button_container: Control = $ButtonContainer
 
+func _ready() -> void:
+	await animation_player.animation_finished
+	game_over_label.show()
+	animation_player.play("game_over")
+	button_container.show()
+	
+	
 func _on_retry_button_pressed() -> void:
 	get_tree().reload_current_scene()
 	
